8495308a8487dc7323f8a8fccf7830c4674e1f91
Tingru Liu <7687@holbertonstudents.com>
2023-10-15 21:33:52 -0700

    q5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - print numbers from 0
+*
+* Description: using the main function
+* this program prints numbers
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
