a686ddcab5f980682e8bcba3f5dfd6fd3806b245
Tingru Liu <7687@holbertonstudents.com>
2023-10-15 21:14:16 -0700

    q3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - print letters from a to z then A to Z
+*
+* Description: using the main function
+* this program prints "a to z and A to Z"
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchat(c);
+	}
+	putchar('\n');
+	return (0);
+}
