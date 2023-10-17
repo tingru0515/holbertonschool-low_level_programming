3f819bd335320b2679ff3130106ea958fdf657a1
Tingru Liu <7687@holbertonstudents.com>
2023-10-17 02:20:34 -0700

    q5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_sign - print the sign of a number
+* @n: is the int that will use for the argument of the function
+* Return: 0
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return(1);
+		printf("+");
+	}
+	else if (n == 0)
+	{
+		return(0);
+		printf("0");
+	}
+	else
+	{
+		return(-1);
+		printf("-");
+	}
+}
