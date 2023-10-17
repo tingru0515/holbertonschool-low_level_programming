66a60105cba3f1ebb67e7ab86cd07d29874dd9b9
Tingru Liu <7687@holbertonstudents.com>
2023-10-17 05:06:27 -0700

    q10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - add 2 integers and return the result
+* @num1: the first integer
+* @num2: the second integer
+* Return: result
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
