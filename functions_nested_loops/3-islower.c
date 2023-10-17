c4ef88973c5717b10c7659faa11055932f952cd7
Tingru Liu <7687@holbertonstudents.com>
2023-10-17 01:34:35 -0700

    q3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower - check if it is lowercase or not
+*
+* Return: 0
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
