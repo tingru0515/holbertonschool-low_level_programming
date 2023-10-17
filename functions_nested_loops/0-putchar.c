b2ce9f95a3e4d9ba277b5a95b37388d30165962a
Tingru Liu <7687@holbertonstudents.com>
2023-10-16 20:51:03 -0700

    q0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,6 +4,13 @@
 *
 * Return: 0
 */
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 int main(void)
 {
 	char putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
@@ -11,7 +18,7 @@ int main(void)
 
 	for (c = 0; c < sizeof(putchar); c++)
 	{
-		_putchar(putchar[c];
+		_putchar(putchar[c]);
 	}
 	_putchar('\n');
 	return (0);
