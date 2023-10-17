6a85a0057806ab4de884a714e840b3ba74614c80
Tingru Liu <7687@holbertonstudents.com>
2023-10-17 04:57:59 -0700

    q9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,14 +10,12 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
-		
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
-			
 			if (prod <= 9)
 			{
 				_putchar(' ');
