ba2f47827b5b658feee8bc86c5a4e04607eed66d
Tingru Liu <7687@holbertonstudents.com>
2023-10-16 21:18:20 -0700

    q0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,7 +8,9 @@
 
 int main(void)
 {
-	int _putchar(char c)                                                            {                                                                               	return (write(1, &c, 1));                                      
+	int _putchar(char c)
+	{
+		return (write(1, &c, 1));
 	}
 
 	_putchar('_');
