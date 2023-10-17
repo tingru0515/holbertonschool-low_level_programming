4cf2fb00669adb6f3a78336b898ccbddb68e30da
Tingru Liu <7687@holbertonstudents.com>
2023-10-17 02:29:00 -0700

    q5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * print_sign - print the sign of a number
 * @n: is the int that will use for the argument of the function
@@ -9,17 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar("+");
 		return(1);
-		printf("+");
 	}
 	else if (n == 0)
 	{
+		_putchar("0");
 		return(0);
-		printf("0");
 	}
 	else
 	{
+		_putchar("-");
 		return(-1);
-		printf("-");
 	}
 }
