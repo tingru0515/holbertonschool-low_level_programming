80e8e8218f871c17a5bdadc76bbdeb449c59a2c6
Tingru Liu <7687@holbertonstudents.com>
2023-10-17 01:06:00 -0700

    q2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabet 10 times in lowercase
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <=9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
