bdf132ded7ef81125dcaa33b4a04d896e1c15c9f
Tingru Liu <7687@holbertonstudents.com>
2023-10-19 00:41:27 -0700

    q3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - print the numbers from 0-9
+*
+* Return: void
+*/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
+}
