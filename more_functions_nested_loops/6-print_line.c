14163d76ea51d96f872bcd538ea0bca734463436
Tingru Liu <7687@holbertonstudents.com>
2023-10-19 01:21:57 -0700

    q6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_line - draws a straight line using the character _
+* @n: the number of _
+*
+* Return: line
+*/
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
