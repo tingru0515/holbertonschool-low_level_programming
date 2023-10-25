a0b622c70bd46cbf8dcf477cd83bb5286b667750
tingru0515 <7687@holbertonstudents.com>
2023-10-25 01:25:34 -0700

    q9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,14 @@
 */
 char *_strcpy(char *dest, const char *src)
 {
-	int index = 0;
-	while (src[index])
+	int i;
+	i = 0;
+
+	while (src[i])
 	{
-		dest[index] = src[index];
-		index++;
+		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
