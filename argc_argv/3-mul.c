00f7ffc4383ff2319eb6109263fbd45e948e6574
tingru0515 <7687@holbertonstudents.com>
2023-10-30 04:29:27 -0700

    q3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints the multiplication of two numbers
+* @argc: the number of arguments
+* @argv: an array of pointers
+* Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2, prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
+	return (0);
+}
