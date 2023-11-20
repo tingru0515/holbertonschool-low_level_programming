dc5dc96199b0510dd12fa8290d64321ec0ae3618
tingru0515 <7687@holbertonstudents.com>
2023-11-19 19:16:36 -0800

    q4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -65,6 +65,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (doggo->name == NULL)
 	{
+		free(doggo);
+		return (NULL);
+	}
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+
+	if (doggo->owner == NULL)
+	{
 		free(doggo->name);
 		free(doggo);
 		return (NULL);
