8c0d11a3b829161fbc6ae7da72b1d62f080a9e82
tingru0515 <7687@holbertonstudents.com>
2023-11-29 15:07:41 -0800

    q1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* list_len - finds the number of elements
+* @h: the linked list_t list
+* Return: the number of elements
+*/
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements ++;
+		h = h->next;
+	}
+	return (elements);
+}
