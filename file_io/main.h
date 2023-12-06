2e80b27b1b3b3253d5827798cb2b633862c968fb
tingru0515 <7687@holbertonstudents.com>
2023-12-06 14:16:53 -0800

    q0

---
+++ file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
