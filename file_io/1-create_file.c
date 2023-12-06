a965497fb4af4067c14191c5849d9fd2a9f2b101
tingru0515 <7687@holbertonstudents.com>
2023-12-06 14:28:49 -0800

    q1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,6 +11,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len] != '\0'; len++)
