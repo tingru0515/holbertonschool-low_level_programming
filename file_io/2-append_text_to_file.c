79942bf2a8a712b88a60af69868208a0b7b06f54
tingru0515 <7687@holbertonstudents.com>
2023-12-06 14:37:02 -0800

    q2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for len = 0; text_content[len] != '\0'; len++)
+		for (len = 0; text_content[len] != '\0'; len++)
 			;
 		w = write(fd, text_content, len);
 		if (w == -1)
