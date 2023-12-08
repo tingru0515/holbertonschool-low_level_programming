d033f4c3ef9c18bc469a777edc60f77ada19b43b
tingru0515 <7687@holbertonstudents.com>
2023-12-07 16:09:57 -0800

    q3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!key || !value || !ht)
 		return (0);
 
-	index = key_index((const unsigned char *)key, size);
+	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
 
 	while (temp)
