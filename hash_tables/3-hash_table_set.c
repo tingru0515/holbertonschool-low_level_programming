b69fa4afe264642a6c99f6c81c465d92af2d59c6
tingru0515 <7687@holbertonstudents.com>
2023-12-07 16:14:27 -0800

    q3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -29,7 +29,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
@@ -39,7 +38,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (0);
 	}
-
 	node->value = strdup(value);
 	if (node->value == NULL)
 	{
@@ -47,7 +45,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (0);
 	}
-
 	node->next = ht->array[index];
 	ht->array[index] = node;
 	return (1);
